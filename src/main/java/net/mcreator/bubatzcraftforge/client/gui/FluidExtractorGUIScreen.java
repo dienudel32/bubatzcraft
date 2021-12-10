eb1e71bd22d5b926a5c4167dcebe190e6c8343d9
dienudel32 <David@DESKTOP-V1FJ2TJ>
2021-12-10 19:43:23 +0100

    UI Updates

--- src/main/java/net/mcreator/bubatzcraftforge/client/gui/FluidExtractorGUIScreen.java
+++ src/main/java/net/mcreator/bubatzcraftforge/client/gui/FluidExtractorGUIScreen.java
@@ -9,31 +9,31 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.Minecraft;
 
-import net.mcreator.bubatzcraftforge.world.inventory.ChemicalmixerGUIMenu;
+import net.mcreator.bubatzcraftforge.world.inventory.FluidExtractorGUIMenu;
 
 import java.util.HashMap;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.systems.RenderSystem;
 
-public class ChemicalmixerGUIScreen extends AbstractContainerScreen<ChemicalmixerGUIMenu> {
+public class FluidExtractorGUIScreen extends AbstractContainerScreen<FluidExtractorGUIMenu> {
 	public final static HashMap<String, Object> guistate = new HashMap<>();
 	private final Level world;
 	private final int x, y, z;
 	private final Player entity;
 
-	public ChemicalmixerGUIScreen(ChemicalmixerGUIMenu container, Inventory inventory, Component text) {
+	public FluidExtractorGUIScreen(FluidExtractorGUIMenu container, Inventory inventory, Component text) {
 		super(container, inventory, text);
 		this.world = container.world;
 		this.x = container.x;
 		this.y = container.y;
 		this.z = container.z;
 		this.entity = container.entity;
-		this.imageWidth = 189;
-		this.imageHeight = 212;
+		this.imageWidth = 176;
+		this.imageHeight = 166;
 	}
 
-	private static final ResourceLocation texture = new ResourceLocation("bubatzcraftforge:textures/chemicalmixer_gui.png");
+	private static final ResourceLocation texture = new ResourceLocation("bubatzcraftforge:textures/fluid_extractor_gui.png");
 
 	@Override
 	public void render(PoseStack ms, int mouseX, int mouseY, float partialTicks) {
@@ -50,11 +50,8 @@ public class ChemicalmixerGUIScreen extends AbstractContainerScreen<Chemicalmixe
 		RenderSystem.setShaderTexture(0, texture);
 		this.blit(ms, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight, this.imageWidth, this.imageHeight);
 
-		RenderSystem.setShaderTexture(0, new ResourceLocation("bubatzcraftforge:textures/chemical_mixer_gui.png"));
-		this.blit(ms, this.leftPos + 29, this.topPos + 1, 0, 0, 128, 128, 128, 128);
-
-		RenderSystem.setShaderTexture(0, new ResourceLocation("bubatzcraftforge:textures/redstone_gui.png"));
-		this.blit(ms, this.leftPos + 158, this.topPos + 103, 0, 0, 16, 16, 16, 16);
+		RenderSystem.setShaderTexture(0, new ResourceLocation("bubatzcraftforge:textures/fluidextractor_gui.png"));
+		this.blit(ms, this.leftPos + 78, this.topPos + 43, 0, 0, 16, 16, 16, 16);
 
 		RenderSystem.disableBlend();
 	}
@@ -75,8 +72,8 @@ public class ChemicalmixerGUIScreen extends AbstractContainerScreen<Chemicalmixe
 
 	@Override
 	protected void renderLabels(PoseStack poseStack, int mouseX, int mouseY) {
-		drawString(poseStack, this.font, "Chemical Mixer", 56, 4, -12829636);
-		drawString(poseStack, this.font, "IDLE", 155, 67, -12829636);
+		drawString(poseStack, this.font, "Fluid Extractor", 51, 7, -12829636);
+		drawString(poseStack, this.font, "IDLE", 51, 43, -12829636);
 	}
 
 	@Override
