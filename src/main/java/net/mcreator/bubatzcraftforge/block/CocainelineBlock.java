59da7d4b7b9e304a1182d5430d4c4432d0bf8bee
dienudel32 <David@DESKTOP-V1FJ2TJ>
2021-12-12 21:24:10 +0100

    cocaine line

---
+++ src/main/java/net/mcreator/bubatzcraftforge/block/CocainelineBlock.java
@@ -0,0 +1,54 @@
+
+package net.mcreator.bubatzcraftforge.block;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.core.BlockPos;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+
+import net.mcreator.bubatzcraftforge.init.BubatzcraftforgeModBlocks;
+
+import java.util.List;
+import java.util.Collections;
+
+public class CocainelineBlock extends Block {
+	public CocainelineBlock() {
+		super(BlockBehaviour.Properties.of(Material.TOP_SNOW).sound(SoundType.SNOW).strength(0f, 10f).noCollission().noOcclusion()
+				.isRedstoneConductor((bs, br, bp) -> false));
+		setRegistryName("cocaineline");
+	}
+
+	@Override
+	public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos) {
+		return true;
+	}
+
+	@Override
+	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+		return 0;
+	}
+
+	@Override
+	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+		if (!dropsOriginal.isEmpty())
+			return dropsOriginal;
+		return Collections.singletonList(new ItemStack(this, 1));
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	public static void registerRenderLayer() {
+		ItemBlockRenderTypes.setRenderLayer(BubatzcraftforgeModBlocks.COCAINELINE, renderType -> renderType == RenderType.cutoutMipped());
+	}
+
+}
--- src/main/java/net/mcreator/bubatzcraftforge/procedures/CocainelineOnBlockRightClickedProcedure.java
+++ src/main/java/net/mcreator/bubatzcraftforge/procedures/CocainelineOnBlockRightClickedProcedure.java
@@ -1,6 +1,14 @@
 package net.mcreator.bubatzcraftforge.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.effect.MobEffectInstance;
 
 public class CocainelineOnBlockRightClickedProcedure {
 	public static void execute(LevelAccessor world, Entity entity) {
