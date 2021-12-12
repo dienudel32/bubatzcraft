be94faec600b06b670ae8c53d106ac170ce76998
dienudel32 <David@DESKTOP-V1FJ2TJ>
2021-12-12 18:52:28 +0100

    tweaked shit

--- src/main/java/net/mcreator/bubatzcraftforge/procedures/MagicmushroomfoodFoodEatenProcedure.java
+++ src/main/java/net/mcreator/bubatzcraftforge/procedures/MagicmushroomfoodFoodEatenProcedure.java
@@ -4,18 +4,23 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.damagesource.DamageSource;
 
 public class MagicmushroomfoodFoodEatenProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
-		if (entity instanceof LivingEntity _entity)
-			_entity.addEffect(new MobEffectInstance(MobEffects.LEVITATION, 1200, 3, (false), (false)));
-		if (entity instanceof LivingEntity _entity)
-			_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 1200, 254, (false), (false)));
-		if (entity instanceof LivingEntity _entity)
-			_entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 1200, 254, (false), (false)));
-		if (entity instanceof LivingEntity _entity)
-			_entity.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 1200, 69, (false), (false)));
+		if (Math.random() < 0.5) {
+			entity.hurt(DamageSource.GENERIC, 5000);
+		} else {
+			if (entity instanceof LivingEntity _entity)
+				_entity.addEffect(new MobEffectInstance(MobEffects.LEVITATION, 1200, 3, (false), (false)));
+			if (entity instanceof LivingEntity _entity)
+				_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 1200, 254, (false), (false)));
+			if (entity instanceof LivingEntity _entity)
+				_entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 1200, 254, (false), (false)));
+			if (entity instanceof LivingEntity _entity)
+				_entity.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 1200, 69, (false), (false)));
+		}
 	}
 }
