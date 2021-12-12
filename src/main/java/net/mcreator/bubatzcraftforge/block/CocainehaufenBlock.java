7f1f1c621a56cb237f390375407094a535bdbe8a
dienudel32 <David@DESKTOP-V1FJ2TJ>
2021-12-12 22:01:09 +0100

    updated mod info for release

--- src/main/java/net/mcreator/bubatzcraftforge/block/CocainehaufenBlock.java
+++ src/main/java/net/mcreator/bubatzcraftforge/block/CocainehaufenBlock.java
@@ -1,16 +1,30 @@
 
 package net.mcreator.bubatzcraftforge.block;
 
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.material.Material;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.core.BlockPos;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+
+import net.mcreator.bubatzcraftforge.init.BubatzcraftforgeModBlocks;
+
+import java.util.List;
+import java.util.Collections;
 
 public class CocainehaufenBlock extends Block {
-
 	public CocainehaufenBlock() {
 		super(BlockBehaviour.Properties.of(Material.TOP_SNOW).sound(SoundType.SNOW).strength(0f, 10f).noCollission().noOcclusion()
 				.isRedstoneConductor((bs, br, bp) -> false));
-
 		setRegistryName("cocainehaufen");
 	}
 
@@ -26,7 +40,6 @@ public class CocainehaufenBlock extends Block {
 
 	@Override
 	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 		if (!dropsOriginal.isEmpty())
 			return dropsOriginal;
