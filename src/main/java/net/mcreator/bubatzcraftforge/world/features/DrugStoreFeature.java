42b0d1a265b673748d35cc6d509bee9b7b56938c
dienudel32 <David@DESKTOP-V1FJ2TJ>
2021-12-19 16:59:08 +0100

    fixed drug shop

--- src/main/java/net/mcreator/bubatzcraftforge/world/features/DrugStoreFeature.java
+++ src/main/java/net/mcreator/bubatzcraftforge/world/features/DrugStoreFeature.java
@@ -23,9 +23,9 @@ public class DrugStoreFeature extends Feature<NoneFeatureConfiguration> {
 	public static final DrugStoreFeature FEATURE = (DrugStoreFeature) new DrugStoreFeature().setRegistryName("bubatzcraftforge:drug_store");
 	public static final ConfiguredFeature<?, ?> CONFIGURED_FEATURE = FEATURE.configured(FeatureConfiguration.NONE);
 	public static final Set<ResourceLocation> GENERATE_BIOMES = Set.of(new ResourceLocation("savanna_plateau"), new ResourceLocation("forest"),
-			new ResourceLocation("giant_tree_taiga"), new ResourceLocation("giant_spruce_taiga"), new ResourceLocation("dark_forest"),
-			new ResourceLocation("taiga"), new ResourceLocation("savanna"), new ResourceLocation("flower_forest"), new ResourceLocation("swamp"),
-			new ResourceLocation("tall_birch_forest"));
+			new ResourceLocation("sunflower_plains"), new ResourceLocation("giant_tree_taiga"), new ResourceLocation("giant_spruce_taiga"),
+			new ResourceLocation("dark_forest"), new ResourceLocation("taiga"), new ResourceLocation("savanna"), new ResourceLocation("plains"),
+			new ResourceLocation("flower_forest"), new ResourceLocation("swamp"), new ResourceLocation("tall_birch_forest"));
 	private StructureTemplate template = null;
 
 	public DrugStoreFeature() {
@@ -52,11 +52,10 @@ public class DrugStoreFeature extends Feature<NoneFeatureConfiguration> {
 				int k = context.origin().getZ() + context.random().nextInt(16);
 				int j = context.level().getHeight(Heightmap.Types.WORLD_SURFACE_WG, i, k);
 				j -= 1;
-				BlockPos spawnTo = new BlockPos(i + 0, j + 0, k + 0);
-				if (template.placeInWorld(context.level(), spawnTo, spawnTo,
-						new StructurePlaceSettings().setMirror(Mirror.values()[context.random().nextInt(2)])
-								.setRotation(Rotation.values()[context.random().nextInt(3)]).setRandom(context.random())
-								.addProcessor(BlockIgnoreProcessor.STRUCTURE_BLOCK).setIgnoreEntities(false),
+				BlockPos spawnTo = new BlockPos(i + 7, j + 0, k + 4);
+				if (template.placeInWorld(
+						context.level(), spawnTo, spawnTo, new StructurePlaceSettings().setMirror(Mirror.NONE).setRotation(Rotation.NONE)
+								.setRandom(context.random()).addProcessor(BlockIgnoreProcessor.STRUCTURE_BLOCK).setIgnoreEntities(false),
 						context.random(), 2)) {
 					anyPlaced = true;
 				}
